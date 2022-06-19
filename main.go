2b6735d6059c0ae0a8f4031cadffbacbc9504b4d
Pawel Czubak <pawel33317@gmail.com>
2022-06-20 00:50:06 +0200

    yscall signals part 1

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/pawel33317/coreCommunicationFramework/app_state_manager"
 	"github.com/pawel33317/coreCommunicationFramework/app_state_manager/app_state"
@@ -30,25 +33,7 @@ func (client *AppStateClient) End(state app_state.State) {
 	client.asm.UnlockState(client)
 }
 
-func main() {
-	db := db_handler.SQLiteDb{}
-	dbErr := db.Open()
-
-	if dbErr != nil {
-		os.Exit(2)
-	}
-	defer db.Close()
-
-	log := logger.NewLoggerImp(&db)
-	log.Enable()
-	log.SetMinLogLevel(logger.DEBUG)
-
-	log.Log(logger.INFO, "MAIN", "App start")
-
-	//dbHandler.RunHandler("test")
-
-	asManager := app_state_manager.NewAppStateManagerImp(log)
-
+func testASMClient(asManager *app_state_manager.AppStateManagerImp, log logger.Logger) {
 	asClient := &AppStateClient{asManager, "A", logger.NewLogWrapper(log, "ASC1")}
 	asClient.Start(app_state.LOADING)
 
@@ -60,4 +45,49 @@ func main() {
 	asClient.End(app_state.LOADING)
 	asClient2.End(app_state.CONFIGURED)
 	log.Log(logger.INFO, "MAIN", "App end")
+}
+
+func main() {
+	db := db_handler.SQLiteDb{}
+	dbErr := db.Open()
+
+	if dbErr != nil {
+		panic(dbErr)
+	}
+	defer db.Close()
+
+	log := logger.NewLoggerImp(&db)
+	log.Enable()
+	log.SetMinLogLevel(logger.DEBUG)
+
+	log.Log(logger.INFO, "MAIN", "App start")
+
+	asManager := app_state_manager.NewAppStateManagerImp(log)
+
+	//napisać wątek, który sobie odlicza sekundy i drugi z odbiorem sygnału kill jak poniżej
+	//czekać na info z obu i zabić apkę na ctrl+c
+	inc := 0
+	for {
+		if inc == 0 {
+			testASMClient(asManager, log)
+			inc++
+		}
+		sigs := make(chan os.Signal, 1)
+		done := make(chan bool, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		go func() {
+			for {
+				sig := <-sigs
+				fmt.Println()
+				fmt.Println(sig)
+				done <- true
+			}
+		}()
+		fmt.Println("awaiting signal")
+		<-done
+		fmt.Println("exiting")
+		os.Exit(1)
+
+	}
+
 }
